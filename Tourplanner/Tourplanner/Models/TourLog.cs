4ff1cad02cbfef5b20c5da19513cf697ca3d44b8
HBK42 <hasankizilirmak42@gmail.com>
2024-02-29 11:03:30 +0100

    .

---
+++ Tourplanner/Tourplanner/Models/TourLog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tourplanner.Models
+{
+    internal class TourLog
+    {
+        public int Id { get; set; } // Consider using a unique identifier
+        public int TourId { get; set; } // Foreign key linking to Tour
+        public DateTime DateTime { get; set; }
+        public string Comment { get; set; }
+        public string Difficulty { get; set; }
+        public double TotalDistance { get; set; }
+        public TimeSpan TotalTime { get; set; }
+        public int Rating { get; set; }
+    }
+}
